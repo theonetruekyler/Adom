94610f306ae30a1ffa7e3dce7f0dfd9d693ba482
Kyler Thompson <kylertheengineer@gmail.com>
2019-01-03 18:09:06 -0500

    added stubs for temperature control implementation.

---
+++ temperature_control.cpp
@@ -0,0 +1,7 @@
+// 
+// 
+// 
+
+#include "temperature_control.h"
+
+
---
+++ temperature_control.h
@@ -0,0 +1,14 @@
+// temperature_control.h
+
+#ifndef _TEMPERATURE_CONTROL_h
+#define _TEMPERATURE_CONTROL_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
