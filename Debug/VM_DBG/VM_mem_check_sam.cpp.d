275acfe6607bdafb5790585f8b7301c3bf809b0f
Kyler Thompson <kylertheengineer@gmail.com>
2019-01-03 18:05:47 -0500

    removed objecy and debug objects from index.

--- Debug/VM_DBG/VM_mem_check_sam.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Kyler\AppData\Local\Temp\VMBuilds\Adom\mega_atmega2560\Debug\VM_DBG\VM_mem_check_sam.cpp.o: \
- c:\program\ files\ (x86)\microsoft\ visual\ studio\2017\community\common7\ide\extensions\rklz3tds.z1r\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp
--- Release/scheduler.c.d
+++
@@ -1,7 +0,0 @@
-C:\Users\Kyler\AppData\Local\Temp\VMBuilds\Adom\yun\Release\scheduler.c.o: \
- C:\Users\Kyler\AppData\Local\Temp\VMBuilds\Adom\yun\Release\scheduler.c \
- C:\Users\Kyler\AppData\Local\Temp\VMBuilds\Adom\yun\Release\scheduler.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\yun/pins_arduino.h \
- c:\program\ files\ (x86)\arduino\hardware\arduino\avr\variants\leonardo\pins_arduino.h
